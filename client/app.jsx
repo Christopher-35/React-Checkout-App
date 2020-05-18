9ed3090cfc0ed7ba624819ebc1eff036b82c5b5d
Chris Roback <RobackChristopher@gmail.com>
2020-05-18 19:43:38 -0400

    updated react

---
+++ client/app.jsx
@@ -0,0 +1,425 @@
+class Form1 extends React.Component  {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.submitForm = this.submitForm.bind(this)
+    this.renderForm2 = this.renderForm2.bind(this);
+  }
+
+
+  state = this.initialState;
+
+  handleChange = e => {
+    const {name, value} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  submitForm = () => {
+    this.props.handleSubmit(this.state)
+    //this.setState(this.initialState);
+
+  }
+
+  renderForm2 = () => {
+    this.submitForm();
+    this.props.handleForm2()
+    console.log('renderForm2')
+  }
+render () {
+  if (!this.props.isFalse){
+    return null;
+  }
+  //const {name, job} = this.state;
+return (
+  <div>
+<Form2 handleSubmit={()=>{this.props.handleSubmit}} handleChange={this.handleChange}
+submitForm={this.submitForm} isFalse={this.state.isFalse} handleForm2={this.handleForm2}
+handleForm3={this.props.handleForm3} isFalse2={this.props.isFalse2} isFalse3={this.props.isFalse3}
+handleForm4={this.props.handleForm4} isFalse4={this.props.isFalse4} state={this.props.state}
+renderParentCallback={this.props.renderParentCallback}/>
+  <form method='POST' action='/f1'>
+    <label>Name</label>
+    <input
+      type='text'
+      name='name'
+      id='name'
+      //value={name}
+      onChange={(e) => this.handleChange(e)}
+    />
+    <br></br>
+    <label>email</label>
+    <input
+      type='text'
+      name='email'
+      id='email'
+      // value={email}
+      onChange={(e) => this.handleChange(e)}
+    />
+    <br></br>
+    <label>password</label>
+    <input
+      type='text'
+      name='password'
+      id='password'
+      // value={password}
+      onChange={(e) => this.handleChange(e)}
+    />
+    <input type="submit" value="Next" onClick={() => this.renderForm2() } />
+  </form>
+  </div>
+  //   {/* F1 collects name, email, and password for account creation. */}
+)
+}
+}
+class Form2 extends React.Component  {
+  constructor (props) {
+    super(props)
+    this.renderForm3 = this.renderForm3.bind(this);
+  }
+
+
+  state = this.initialState;
+
+  renderForm3 = () => {
+    this.props.submitForm();
+    this.props.handleForm3()
+    console.log('renderForm3')
+  }
+
+render () {
+  if (!this.props.isFalse2){
+    return null;
+  }
+  //const {name, job} = this.state;
+return (
+  <div>
+    <Form3 handleSubmit={this.props.handleSubmit} handleChange={this.props.handleChange}
+submitForm={this.props.submitForm} isFalse={this.props.isFalse} handleForm3={this.props.handleForm3}
+isFalse3={this.props.isFalse3} handleForm4={this.props.handleForm4} isFalse4={this.props.isFalse4}
+state={this.props.state} renderParentCallback={this.props.renderParentCallback}/>
+  <form method="POST" action="/f2">
+    <label>Address1</label>
+    <input
+      type='text'
+      name='Address1'
+      id='Address1'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>Address2</label>
+    <input
+      type='text'
+      name='Address2'
+      id='Address2'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>City</label>
+    <input
+      type='text'
+      name='City'
+      id='City'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>State</label>
+    <input
+      type='text'
+      name='State'
+      id='State'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>zipcode</label>
+    <input
+      type='text'
+      name='zipcode'
+      id='zipcode'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>Phone Number</label>
+    <input
+      type='text'
+      name='phoneNumber'
+      id='phoneNumber'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <input type="submit" value="Next" onClick={() => {this.renderForm3()}} />
+  </form>
+  </div>
+  //ine 1, line 2, city, state, zip code) and phone number
+)
+}
+}
+
+class Form3 extends React.Component  {
+  constructor (props) {
+    super(props)
+    this.renderForm4 = this.renderForm4.bind(this);
+  }
+
+
+  state = this.initialState;
+
+  renderForm4 = () => {
+    this.props.submitForm();
+    this.props.handleForm4();
+
+  }
+
+render () {
+  if (!this.props.isFalse3){
+    return null;
+  }
+
+return (
+  <div>
+    <Form4 handleSubmit={this.props.handleSubmit} handleChange={this.props.handleChange}
+submitForm={this.props.submitForm} isFalse={this.props.isFalse} handleForm3={this.props.handleForm3}
+isFalse3={this.props.isFalse3} handleForm4={this.props.handleForm4} isFalse4={this.props.isFalse4}
+state={this.props.state} renderParentCallback={this.props.renderParentCallback}/>
+  <form method="POST" action="/f3">
+    <label>cardNo.</label>
+    <input
+      type='text'
+      name='cardNo'
+      id='cardNo'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>expirationDate</label>
+    <input
+      type='text'
+      name='expirationDate'
+      id='expirationDate'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>cvv</label>
+    <input
+      type='text'
+      name='cvv'
+      id='cvv'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+    <br></br>
+    <label>billingZipCode</label>
+    <input
+      type='text'
+      name='billingZipCode'
+      id='billingZipCode'
+      onChange={(e) => this.props.handleChange(e)}
+    />
+
+    <input type="submit" value="Next" onClick={() => this.renderForm4()} />
+  </form>
+  </div>
+  //credit card #, expiry date, CVV, and billing zip code
+)
+}
+}
+
+class Form4 extends React.Component  {
+  constructor (props) {
+    super(props)
+  }
+
+
+  state = this.initialState;
+
+render () {
+  if (!this.props.isFalse4) {
+    return null;
+  }
+
+return (
+  <div>
+    {/*console.log(this.props.state[this.props.state.length-1].name)*/}
+  <form>
+
+    <label>Confirm Your Info</label>
+    <br></br>
+{/* {`Name:${this.state.characters[this.state.characters.length-1].name} Name:${} Name:${} Name:${} Name:${} Name:${} Name:${} Name:${} Name:${} Name:${} Name:${}
+`} */} {/*Name: + {this.state.characters[this.state.characters.length-1].name}*/}
+ Name: {this.props.state[this.props.state.length-1].name} <br></br>
+ Email: {this.props.state[this.props.state.length-1].email}<br></br>
+ Password: {this.props.state[this.props.state.length-1].password}<br></br>
+ Address1: {this.props.state[this.props.state.length-1].Address1}<br></br>
+ Address2: {this.props.state[this.props.state.length-1].Address2}<br></br>
+ City: {this.props.state[this.props.state.length-1].City}<br></br>
+ zipcode: {this.props.state[this.props.state.length-1].zipcode}<br></br>
+ cardNo.: {this.props.state[this.props.state.length-1].cardNo}<br></br>
+ Expiration Date: {this.props.state[this.props.state.length-1].expirationDate}<br></br>
+ CVV: {this.props.state[this.props.state.length-1].cvv}<br></br>
+ Billing-ZipCode: {this.props.state[this.props.state.length-1].billingZipCode}<br></br>
+ Phone-Number: {this.props.state[this.props.state.length-1].phoneNumber}<br></br>
+{/* {1 collects name, email, and password for account creation.
+F2 collects ship to address (line 1, line 2, city, state, zip code) and phone number.
+F3 collects credit card #, expiry date, CVV, and billing zip code.} */}
+    <input type="button" value="Purchase" onClick={() => this.props.renderParentCallback()} />
+  </form>
+  </div>
+  //credit card #, expiry date, CVV, and billing zip code
+)
+}
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      //for button
+      isDisabled: false,
+      isFalse: false,
+      isFalse2: false,
+      isFalse3: false,
+      isFalse4: false,
+      //to store input values
+      characters: []
+    }
+    // this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleForm2 = this.handleForm2.bind(this);
+    this.handleForm3 = this.handleForm3.bind(this);
+    this.handleForm4 = this.handleForm4.bind(this);
+    this.renderParentCallback = this.renderParentCallback.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(char) {
+    // event.preventDefault();
+    this.setState({characters: [...this.state.characters, char]})
+  }
+
+  handleClick(){
+
+    this.setState({isFalse: !this.state.isFalse, isDisabled: !this.state.isDisabled})
+  }
+
+  handleForm2(){
+    this.setState({isFalse2: !this.state.isFalse2})
+    console.log('APP handleForm2')
+  }
+
+  handleForm3(){
+    this.setState({isFalse3: !this.state.isFalse3})
+    console.log('APP handleForm3')
+  }
+
+  handleForm4(){
+    this.setState({isFalse4: !this.state.isFalse4})
+  }
+
+  renderParentCallback() {
+    this.setState({isDisabled: !this.state.isDisabled, isFalse: !this.state.isFalse, isFalse2: !this.state.isFalse2, isFalse3: !this.state.isFalse3, isFalse4: !this.state.isFalse4})
+    this.forceUpdate();
+  }
+
+  render() {
+
+    return (
+      <div>
+       {/* <form onSubmit={this.handleSubmit} method='POST'action='/'> */}
+      <Form1 handleSubmit={this.handleSubmit} isFalse={this.state.isFalse} click={this.handleClick}
+      handleForm2={this.handleForm2} handleForm3={this.handleForm3} isFalse2={this.state.isFalse2}
+      isFalse3={this.state.isFalse3} handleForm4={this.handleForm4} isFalse4={this.state.isFalse4}
+      state={this.state.characters} renderParentCallback={this.renderParentCallback}/>
+
+        <form method="POST" action="/home" >
+        <input type="submit" value="Checkout"  onClick={() => this.handleClick() } disabled={this.state.isDisabled}/>
+
+        </form>
+
+        </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
+
+
+// class App extends React.Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//       fullName: '',
+//       email: '',
+//       password: '',
+//     }
+//     this.handleSubmit.bind(this);
+//     this.handleInputChange.bind(this);
+//   }
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//   }
+
+//   handleInputChange (e) => {
+//     e.preventDefault();
+//     this.setState({
+//       //e.target.name works for all inputs
+//       [e.target.name]: e.target.value
+//     })
+//   }
+//     // this.handleChange = this.handleChange.bind(this);
+//     // this.state = {
+//     //   // "DataSource" is some global data source
+//     //   comments: DataSource.getComments()
+//     // };
+
+
+
+//   // componentDidMount() {
+//   //   // Subscribe to changes
+//   //   DataSource.addChangeListener(this.handleChange);
+//   // }
+
+//   // componentWillUnmount() {
+//   //   // Clean up listener
+//   //   DataSource.removeChangeListener(this.handleChange);
+//   // }
+
+//   // handleChange() {
+//   //   // Update component state whenever the data source changes
+//   //   this.setState({
+//   //     comments: DataSource.getComments()
+//   //   });
+//   // }
+
+//   render() {
+//     const {fullName} = this.state
+//     return (
+//       <div>
+//         {/* {this.state.comments.map((comment) => (
+//           <Comment comment={comment} key={comment.id} />
+//         ))} */}
+//         <form onSubmit={this.handleSubmit}>
+//           <input
+//           type='text'
+//           name='fullName'
+//           placeholder='name'
+//           value={this.state.name}
+//           onChange={(e) => this.handleInputChange(e)}
+//           />
+
+//         </form>
+//         dfsdzsgvvs
+//       </div>
+//     );
+//   }
+// }
---
+++ server.js
@@ -0,0 +1,164 @@
+//SERVER------------------------------------
+//  REMEMBER YOU NEED A BABEL RC FILE
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: true })
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+//DATABASE---------------------------------------
+const mysql = require('mysql');
+
+
+
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'sparkie1218',
+  database: 'userinfo'
+})
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+
+
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('short'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.get('/', (req, res) => {
+//   console.log('firstGet')
+// })
+
+app.post('/home', (req, res) => {
+console.log('post hit');
+let arr =["test1", "test2", "test3"];
+  connection.query(`INSERT INTO checkouts(name, email, password)VALUES (?) `, [arr], function(err, result){
+    if(err){
+      console.log("ERROR--->", err);
+    };
+  });
+})
+
+// app.get('/f1', (req, res) => {
+//   console.log('get2', req.body);
+// })
+
+app.post('/f1', (req, res) => {
+  console.log('req.query==', req.query);
+  if (req.body.name && req.body.email && req.body.password){
+    let name= req.body.name + "";
+    let email=req.body.email + "";
+    let password=req.body.password + "";
+    let arr =[];
+    arr.push(name, email, password);
+    connection.query(`INSERT INTO checkouts(name, email, password)VALUES (?) `, [arr], function(err, result){
+      if(err)
+          console.log(`ERROR--->`, err);
+      });
+    }
+
+
+
+
+})
+
+// app.get('/f2', (req, res) => {
+
+// })
+
+app.post('/f2', (req, res) => {
+  console.log('inF2');
+  if (req.body.Address1 && req.body.Address2 && req.body.City && req.body.State && req.body.zipcode && req. body.phoneNumber ) {
+
+    let Address1= req.body.Address1 + "";
+    let Address2=req.body.Address2 + "";
+    let City=req.body.City + "";
+    let State = req.body.State + "";
+    let zipcode = req.body.zipcode + "";
+    let phoneNumber = req.body.phoneNumber + "";
+    let arr =[];
+
+    arr.push(Address1, Address2, City, State, zipcode, phoneNumber);
+
+    connection.query(`SELECT MAX(id) FROM checkouts`, function(err, lastId){
+      if (err) {
+        console.log(`error--->`, err);
+      }
+      lastId = lastId[0][`MAX(id)`];
+      console.log('lastiD===', typeof(lastId));
+    connection.query(`UPDATE checkouts SET address1=?, address2=?, city=?, state=?, zipcode=?, phonenum=? WHERE id=${lastId}`, arr, function(err, result){
+      if (err) {
+          console.log(`ERROR--->`, err);
+      };
+      console.log('result===', result);
+  })
+
+    })
+
+}
+// var sql = "UPDATE trn_employee set first_name =? , last_name =?  WHERE employee_id = ?";
+
+// var query = connection.query(sql, ["Vinayak", "Patil", 1], function(err, result) {
+//     console.log("Record Updated!!");
+//     console.log(result);
+
+
+});
+
+// app.get('/f3', (req, res) => {
+// })
+
+app.post('/f3', (req, res) => {
+  console.log('INF3');
+
+  if (req.body.cardNo && req.body.expirationDate && req.body.cvv && req.body.billingZipCode) {
+
+    let cardNo = req.body.cardNo + "";
+    let expirationDate = req.body.expirationDate + "";
+    let cvv = req.body.cvv + "";
+    let billingZipCode = req.body.billingZipCode + "";
+
+    //creditno | expirdata | cvv  | billingzip
+    let arr =[];
+    arr.push(cardNo, expirationDate, cvv, billingZipCode);
+
+    connection.query(`SELECT MAX(id) FROM checkouts`, function(err, lastId){
+      if (err) {
+        console.log(`error--->`, err);
+      }
+      lastId = lastId[0][`MAX(id)`];
+
+//`UPDATE checkouts SET address1=?, address2=?, city=?, state=?, zipcode=?, phonenum=? WHERE id=${lastId}`
+    connection.query(`UPDATE checkouts SET creditno=?, expirdata=?, cvv=?, billingzip=? WHERE id=${lastId}`, arr, function(err, result){
+      if (err) {
+          console.log(`ERROR--->`, err);
+      };
+
+})
+
+    })
+}
+
+})
+
+ app.listen(port, () => console.log('server running...'));
+
+
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+ module.exports = connection;
+
